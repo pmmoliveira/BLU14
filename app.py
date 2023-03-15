bff32866d71d5dfdeab9a4536973205b785f1a5e
Pedro Oliveira <83532161+pmmoliveira@users.noreply.github.com>
2023-03-15 20:50:15 +0000

    Update app.py

--- app.py
+++ app.py
@@ -58,58 +58,58 @@ app = Flask(__name__)
 @app.route('/predict', methods=['POST'])
 def predict():
     #obs_dict = request.get_json()
-    request = request.get_json()
+    request_dict = request.get_json()
     base_dict_keys = ['observation_id', 'data']
     data_dict_keys = ['age','workclass','sex','race','education','marital-status', 'capital-gain', 
                       'capital-loss', 'hours-per-week']
     category_columns = ['workclass','sex','race','education','marital-status']
     numeric_colums = ['age', 'capital-gain', 'capital-loss', 'hours-per-week']
-    if list(request.keys())[0] != base_dict_keys[0]:
+    if list(request_dict.keys())[0] != base_dict_keys[0]:
         return jsonify({
                 "observation_id": None,
                 "error": "Missing observation_id"
                     })
-    if list(request.keys())[1] != base_dict_keys[1]:
+    if list(request_dict.keys())[1] != base_dict_keys[1]:
         return jsonify({
                 "observation_id": None,
                 "error": "Missing data"
                     })
     for key in data_dict_keys:
-        if key not in list(request['data'].keys()):
+        if key not in list(request_dict['data'].keys()):
             return jsonify({
-                "observation_id": request['observation_id'],
+                "observation_id": request_dict['observation_id'],
                 "error": "Missing " + key
                     })
-    for key in list(request['data'].keys()):
+    for key in list(request_dict['data'].keys()):
         if key not in data_dict_keys:
             return jsonify({
-                "observation_id": request['observation_id'],
+                "observation_id": request_dict['observation_id'],
                 "error": key + ' not recognized'
                     })
     for cat in category_columns:
-        if request['data'][cat] not in get_valid_categories(df, cat):
+        if request_dict['data'][cat] not in get_valid_categories(df, cat):
             return jsonify({
-                    "observation_id": request['observation_id'],
-                    "error": request['data'][cat] + " incorrect value for " + cat
+                    "observation_id": request_dict['observation_id'],
+                    "error": request_dict['data'][cat] + " incorrect value for " + cat
                     })
     for cat in numeric_colums:
-        if (request['data'][cat] > df[cat].max()) or (request['data'][cat] < df[cat].min()):
+        if (request_dict['data'][cat] > df[cat].max()) or (request_dict['data'][cat] < df[cat].min()):
             return jsonify({
-                    "observation_id": request['observation_id'],
-                    "error": str(request['data'][cat]) + " incorrect value for " + cat
+                    "observation_id": request_dict['observation_id'],
+                    "error": str(request_dict['data'][cat]) + " incorrect value for " + cat
                     })
     
     #### Prediction
     pred_prob = pipeline.predict_proba(pd.DataFrame([{
-            "age": request['data']['age'], 
-            "workclass": request['data']['workclass'], 
-            "education": request['data']['education'], 
-            "marital-status": request['data']['marital-status'], 
-            "race": request['data']['race'],
-            "sex": request['data']['sex'],
-            "capital-gain": request['data']['capital-gain'], 
-            "capital-loss": request['data']['capital-loss'], 
-            "hours-per-week": request['data']['hours-per-week']}
+            "age": request_dict['data']['age'], 
+            "workclass": request_dict['data']['workclass'], 
+            "education": request_dict['data']['education'], 
+            "marital-status": request_dict['data']['marital-status'], 
+            "race": request_dict['data']['race'],
+            "sex": request_dict['data']['sex'],
+            "capital-gain": request_dict['data']['capital-gain'], 
+            "capital-loss": request_dict['data']['capital-loss'], 
+            "hours-per-week": request_dict['data']['hours-per-week']}
         ], columns=columns).astype(dtypes))[0][1]
     
     if pred_prob > 0.5:
@@ -117,7 +117,7 @@ def predict():
     else:
         pred = False
     response = {
-                "observation_id": request['observation_id'],
+                "observation_id": request_dict['observation_id'],
                 "prediction": pred,
                 "probability": pred_prob
             }
