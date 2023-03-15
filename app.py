3544ae9c700133899bc9544a367b98a2f0666b58
Pedro Oliveira <83532161+pmmoliveira@users.noreply.github.com>
2023-03-15 21:25:14 +0000

    Update app.py

--- app.py
+++ app.py
@@ -13,10 +13,10 @@ from playhouse.db_url import connect
 
 #####
 # Data import
-df = pd.read_csv("bank.csv")
+#df = pd.read_csv("bank.csv")
 
 #####
-def get_valid_categories(df, column):
+#def get_valid_categories(df, column):
     """
     Obtain list of available categories for column
     
@@ -27,9 +27,9 @@ def get_valid_categories(df, column):
     Returns:
         categories: A list of potential values for column
     """
-    categories = list(df[column].unique())
+ #   categories = list(df[column].unique())
 
-    return categories
+ #   return categories
 
 
 ########################################
@@ -59,45 +59,45 @@ app = Flask(__name__)
 def predict():
     #obs_dict = request.get_json()
     request_dict = request.get_json()
-    base_dict_keys = ['observation_id', 'data']
-    data_dict_keys = ['age','workclass','sex','race','education','marital-status', 'capital-gain', 
-                      'capital-loss', 'hours-per-week']
-    category_columns = ['workclass','sex','race','education','marital-status']
-    numeric_colums = ['age', 'capital-gain', 'capital-loss', 'hours-per-week']
-    if list(request_dict.keys())[0] != base_dict_keys[0]:
-        return jsonify({
-                "observation_id": None,
-                "error": "Missing observation_id"
-                    })
-    if list(request_dict.keys())[1] != base_dict_keys[1]:
-        return jsonify({
-                "observation_id": None,
-                "error": "Missing data"
-                    })
-    for key in data_dict_keys:
-        if key not in list(request_dict['data'].keys()):
-            return jsonify({
-                "observation_id": request_dict['observation_id'],
-                "error": "Missing " + key
-                    })
-    for key in list(request_dict['data'].keys()):
-        if key not in data_dict_keys:
-            return jsonify({
-                "observation_id": request_dict['observation_id'],
-                "error": key + ' not recognized'
-                    })
-    for cat in category_columns:
-        if request_dict['data'][cat] not in get_valid_categories(df, cat):
-            return jsonify({
-                    "observation_id": request_dict['observation_id'],
-                    "error": request_dict['data'][cat] + " incorrect value for " + cat
-                    })
-    for cat in numeric_colums:
-        if (request_dict['data'][cat] > df[cat].max()) or (request_dict['data'][cat] < df[cat].min()):
-            return jsonify({
-                    "observation_id": request_dict['observation_id'],
-                    "error": str(request_dict['data'][cat]) + " incorrect value for " + cat
-                    })
+    #base_dict_keys = ['observation_id', 'data']
+    #data_dict_keys = ['age','workclass','sex','race','education','marital-status', 'capital-gain', 
+    #                  'capital-loss', 'hours-per-week']
+    #category_columns = ['workclass','sex','race','education','marital-status']
+    #numeric_colums = ['age', 'capital-gain', 'capital-loss', 'hours-per-week']
+    #if list(request_dict.keys())[0] != base_dict_keys[0]:
+    #    return jsonify({
+    #            "observation_id": None,
+    #            "error": "Missing observation_id"
+    #                })
+    #if list(request_dict.keys())[1] != base_dict_keys[1]:
+    #    return jsonify({
+    #            "observation_id": None,
+    #            "error": "Missing data"
+    #                })
+    #for key in data_dict_keys:
+    #    if key not in list(request_dict['data'].keys()):
+    #        return jsonify({
+    #            "observation_id": request_dict['observation_id'],
+    #            "error": "Missing " + key
+    #                })
+    #for key in list(request_dict['data'].keys()):
+    #    if key not in data_dict_keys:
+    #        return jsonify({
+    #            "observation_id": request_dict['observation_id'],
+    #            "error": key + ' not recognized'
+    #                })
+    #for cat in category_columns:
+    #    if request_dict['data'][cat] not in get_valid_categories(df, cat):
+    #        return jsonify({
+    #                "observation_id": request_dict['observation_id'],
+    #                "error": request_dict['data'][cat] + " incorrect value for " + cat
+    #                })
+    #for cat in numeric_colums:
+    #    if (request_dict['data'][cat] > df[cat].max()) or (request_dict['data'][cat] < df[cat].min()):
+    #        return jsonify({
+    #                "observation_id": request_dict['observation_id'],
+    #                "error": str(request_dict['data'][cat]) + " incorrect value for " + cat
+    #                })
     
     #### Prediction
     pred_prob = pipeline.predict_proba(pd.DataFrame([{
